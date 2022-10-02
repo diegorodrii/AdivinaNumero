327686000d7e5aed6a35f0f71101fdd03bb48055
diegorodrii <diegomiragenil@gmail.com>
2022-10-02 12:34:53 +0200

    Ejercicio finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  numAleatorio(a, b): number {
+    return Math.round(Math.random() * (b - a));
+  }
+  num: number;
+  numSecret: number = this.numAleatorio(0, 100);
+  mayorMenor= "...";
 
+  constructor() { }
+
+
+  comprobacionNumero() {
+    if (this.num) {
+      if (this.numSecret < this.num) {
+        this.mayorMenor = 'menor que';
+      }
+      else if (this.numSecret > this.num) {
+        this.mayorMenor = 'mayor que';
+      }
+      else {
+        this.mayorMenor = '';
+      }
+    }
+  }
+  reinicia(){
+    // reiniciamos las variables
+    this.num = null;
+    this.mayorMenor = '...';
+    this.numSecret = this.numAleatorio(0,100);
+  }
 }
