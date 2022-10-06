cb64d01340672619fe4e67a084678085c40a3446
diegorodrii <diegomiragenil@gmail.com>
2022-10-06 12:31:17 +0200

    Empleo de ngstyle y ngclass

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-interface Number{
-  num:number;
+interface Number {
+  num: number;
 }
 
 @Component({
@@ -18,39 +18,45 @@ export class HomePage {
   }
   num: number;
   numeroSecreto: number = this.numAleatorio(0, 100);
-  menorMayor= "...";
+  menorMayor = "...";
   intentos: number = 0;
+  aciertaNum : boolean;
 
-  array:Number []= [];
+  array: Number[] = [];
   constructor() { }
-  
-  history(_num){
+
+  history(_num) {
     this.array.push(_num);
   }
-  
+
 
   comprobacionNumero() {
- 
 
-      if (this.numeroSecreto < this.num) {
-        this.menorMayor = 'menor que';
-      }
-      else if (this.numeroSecreto > this.num) {
-        this.menorMayor = 'mayor que';
-      }
-      else {
-        this.menorMayor = '';
-      }
-      this.intentos++;
+
+    if (this.numeroSecreto < this.num) {
+      this.menorMayor = 'menor que';
+    }
+    else if (this.numeroSecreto > this.num) {
+      this.menorMayor = 'mayor que';
+    }
+    else {
+      this.menorMayor = '';
+      this.aciertaNum = true;
 
     }
-  
-  reinicia(){
+    this.intentos++;
+
+  }
+
+  reinicia() {
     this.num = null;
-    this.menorMayor= '...';
-    this.numeroSecreto = this.numAleatorio(0,100);
+    this.menorMayor = '...';
+    this.numeroSecreto = this.numAleatorio(0, 100);
     this.intentos = 0;
 
+    
+  }
+  cambiaColor(){
 
   }
 }
